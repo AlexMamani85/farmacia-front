1166ad09d0fd306e4332735a8ccaffac356712f3
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-07-24 08:56:21 -0500

    CHORE: update-form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Products from './components/products'
 import CreateForm from './components/create-form.js'
+import UpdateForm from './components/update-form.js'
 function App() {
   return (
     <Routes>
       <Route path="/" element={<></>} />
       <Route path="/products" element={<Products />} />
       <Route path="/create" element={<CreateForm />} />
+      <Route path="/update" element={<UpdateForm />} />
 
       
     </Routes>
